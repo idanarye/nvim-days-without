4362be48c11435153f8e46a77d61c276bfa76618
Idan Arye <IdanArye@gmail.com>
2024-03-20 19:58:42 +0200

    Support detection for `git` repositories

--- lua/days-without/detection.lua
+++ lua/days-without/detection.lua
@@ -1,3 +1,41 @@
 local M = {}
 
+local util = require'days-without.util'
+
+function M.detect_git(path)
+    local touched_files = {}
+    for _, filename in ipairs(util.run_shell_cmd { cwd = path, 'git', 'diff', '--name-only' }) do
+        touched_files[filename] = true
+    end
+    for _, filename in ipairs(util.run_shell_cmd { cwd = path, 'git', 'diff', '--name-only', '--cached' }) do
+        touched_files[filename] = true
+    end
+
+    local current_time = vim.loop.gettimeofday()
+    local oldest
+
+    if next(touched_files) then
+        local co = util.enforce_coroutine()
+        oldest = current_time
+
+        for filename in pairs(touched_files) do
+            vim.loop.fs_stat(vim.fs.normalize(path .. '/' .. filename), function(err, stat)
+                assert(not err)
+                touched_files[filename] = nil
+                if stat.mtime.sec < oldest then
+                    oldest = stat.mtime.sec
+                end
+                if next(touched_files) == nil then
+                    coroutine.resume(co)
+                end
+            end)
+        end
+        coroutine.yield()
+    else
+        oldest = tonumber(util.run_shell_cmd { cwd = path, 'git', 'log', '-1', '--format=%ct' }[1])
+    end
+
+    return current_time - oldest
+end
+
 return M
--- lua/days-without/util.lua
+++ lua/days-without/util.lua
@@ -1,3 +1,49 @@
 local M = {}
 
+function M.enforce_coroutine()
+    local co = coroutine.running()
+    assert(co, 'Must run inside a coroutine')
+    return co
+end
+
+local function pipe_reader(pipe)
+    local chunks = {}
+    vim.loop.read_start(pipe, function(err, data)
+        assert(not err)
+        table.insert(chunks, data)
+    end)
+    return chunks
+end
+
+function M.run_shell_cmd(cmd)
+    local co = M.enforce_coroutine()
+
+    local stdout = vim.loop.new_pipe()
+    local stderr = vim.loop.new_pipe()
+
+    vim.loop.spawn(cmd[1], {
+        cwd = cmd.cwd,
+        args = {unpack(cmd, 2)},
+        stdio = {nil, stdout, stderr},
+    }, function(exit_code)
+        coroutine.resume(co, exit_code)
+    end)
+
+    local out_chunks = pipe_reader(stdout)
+    local err_chunks = pipe_reader(stderr)
+
+    local exit_code = coroutine.yield()
+
+    if exit_code == 0 then
+        local result = vim.split(table.concat(out_chunks), '\n')
+        local last_idx = #result
+        if result[last_idx] == '' then
+            result[last_idx] = nil
+        end
+        return result
+    else
+        error(table.concat(err_chunks))
+    end
+end
+
 return M
