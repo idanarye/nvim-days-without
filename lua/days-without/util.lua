b696a32a5bf9445653e7cea2feef995a3a50dde7
Idan Arye <IdanArye@gmail.com>
2024-03-21 13:15:13 +0200

    Improve robustness

--- lua/days-without/util.lua
+++ lua/days-without/util.lua
@@ -1,5 +1,21 @@
 local M = {}
 
+function M.launch_coroutine(dlg, ...)
+    local co = coroutine.create(function(...)
+        xpcall(dlg, function(err)
+            if type(err) ~= 'string' then
+                err = vim.inspect(err)
+            end
+            local traceback = debug.traceback(err, 2)
+            traceback = string.gsub(traceback, '\t', string.rep(' ', 8))
+            vim.notify(traceback, vim.log.levels.ERROR, {
+                title = 'ERROR in a "Days Without" related coroutine'
+            })
+        end, ...)
+    end)
+    coroutine.resume(co, ...)
+end
+
 function M.enforce_coroutine()
     local co = coroutine.running()
     assert(co, 'Must run inside a coroutine')
@@ -21,13 +37,16 @@ function M.run_shell_cmd(cmd)
     local stdout = vim.loop.new_pipe()
     local stderr = vim.loop.new_pipe()
 
-    vim.loop.spawn(cmd[1], {
-        cwd = cmd.cwd,
+    local handle, err = vim.loop.spawn(cmd[1], {
+        cwd = vim.fn.fnamemodify(cmd.cwd, ':p'),
         args = {unpack(cmd, 2)},
         stdio = {nil, stdout, stderr},
     }, function(exit_code)
         coroutine.resume(co, exit_code)
     end)
+    if not handle then
+        error(err)
+    end
 
     local out_chunks = pipe_reader(stdout)
     local err_chunks = pipe_reader(stderr)
